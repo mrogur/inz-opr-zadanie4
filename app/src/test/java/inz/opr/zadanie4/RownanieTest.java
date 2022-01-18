84d014c8ea199e1c4f0fa11105540bd002b48a9a
Pawel Wlazlo <mrogur@gmail.com>
2022-01-18 22:05:23 +0100

    dodanie klasy głównego algorytmu

---
+++ app/src/test/java/inz/opr/zadanie4/RownanieTest.java
@@ -0,0 +1,19 @@
+package inz.opr.zadanie4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class RownanieTest {
+    @ParameterizedTest
+    @CsvSource({"1, -3, 5, -1, 4",
+                "1, 4, 3, -3, -1"})
+    void shouldReturnTwo(double a, double b, double c, double x1, double x2) {
+        var rownanie = new Rownanie();
+        var result = rownanie.oblicz(a, b, c);
+        assertArrayEquals(new double[] {x1, x2}, result);
+    }
+}
