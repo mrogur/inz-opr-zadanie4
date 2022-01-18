df43620a07eed5ffcd653fd4cc53b009b64defe2
Pawel Wlazlo <mrogur@gmail.com>
2022-01-18 22:26:05 +0100

    optymizacja importów

--- app/src/test/java/inz/opr/zadanie4/RownanieTest.java
+++ app/src/test/java/inz/opr/zadanie4/RownanieTest.java
@@ -1,11 +1,9 @@
 package inz.opr.zadanie4;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RownanieTest {
     @ParameterizedTest
