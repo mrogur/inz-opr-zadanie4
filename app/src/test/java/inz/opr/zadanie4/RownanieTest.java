eebb2283c40d5f6ff23441f9ce92cb4866ed5599
Pawel Wlazlo <mrogur@gmail.com>
2022-01-19 15:25:08 +0100

    dodanie drugiego case do testu

--- app/src/test/java/inz/opr/zadanie4/RownanieTest.java
+++ app/src/test/java/inz/opr/zadanie4/RownanieTest.java
@@ -7,7 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class RownanieTest {
     @ParameterizedTest
-    @CsvSource({"1, 4, 3, -3, -1"})
+    @CsvSource({"1, 4, 3, -3, -1",
+                "1, 3, -4, -4, 1"})
     void shouldReturnTwo(double a, double b, double c, double x1, double x2) {
         var rownanie = new Rownanie();
         var result = rownanie.oblicz(a, b, c);
